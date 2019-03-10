a3b2d681a5b820a1264e159519df119599a56f2f
Егор Матвеев <emmatveev_1@edu.hse.ru>
2019-03-10 11:51:36 +0300

    Убрал лишние комменты

--- XBot/XBot/Bot.cs
+++ XBot/XBot/Bot.cs
@@ -59,10 +59,8 @@ namespace XBot
                         string[] first = elems[i].Split('\t');
                         string info = await client.GetStringAsync("http://mediametrics.ru/rating/index.tsv?titles=" + first[5]);
                         string[] elems1 = info.Split('\t');
-                        string article = elems1[1];
-                        string link = elems1[0];
                         foreach (string str in requests)
-                            if (article.ToLower().Contains(str.ToLower()))
+                            if (elems1[1].ToLower().Contains(str.ToLower()))
                             {
                                 mess += elems1[1] + "\n֍" + elems1[0] + "֍";
                                 amount++;
@@ -86,6 +84,10 @@ namespace XBot
             m.Display();
         }
 
+        //Быстрый поиск, пока откладывается
+
+
+        /*
         async public static void FastSearch(MainPage m, string str)
         {
             List<string> mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
@@ -132,81 +134,6 @@ namespace XBot
             m.MakeFrame();
             m.MakeContent();
         }
-
-
-
-        /*
-        //public static string news;
-        //static string sender = "https://api.telegram.org/bot710143249:AAHCpMfVfSKgoRdwAnfVJ1j3N8LAEVUyLG4/";
-        //static string receiver = "https://api.telegram.org/bot716832978:AAFENDh7xeOZot_wVAWJ3XMBROUmmY6NLhU/";
-        public static void Activate(MainPage m)
-        {
-            GetUrl();
-            List<string> mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-            mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-            mes.Add("BБот готов к работе");
-            App.Current.Properties["messages"] = Formats.FromListIntoString(mes);
-            m.MakeFrame();
-            m.MakeContent();
-        }
-
-        async static void GetUrl()
-        {
-            news = await client.GetStringAsync("https://api.telegram.org/bot716832978:AAFENDh7xeOZot_wVAWJ3XMBROUmmY6NLhU/getUpdates");
-        }
-
-        async public static void send_mes(string mes)
-        {
-            news = await client.GetStringAsync(sender + "sendMessage?chat_id=-1001411015706&text=" + mes);
-        }
-
-        async public static void check(MainPage m)
-        {
-            try
-            {
-                while (true)
-                {
-                    bool flag = true;
-                    string new_news = "";
-                    try { new_news = await client.GetStringAsync("https://api.telegram.org/bot716832978:AAFENDh7xeOZot_wVAWJ3XMBROUmmY6NLhU/getUpdates"); }
-                    catch
-                    {
-                        flag = false;
-                    }
-                    List<string> mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-                    if ((new_news.Length != news.Length) && flag)
-                    {
-                        mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-                        string line = await client.GetStringAsync("http://mediametrics.ru/rating/ru/online.tsv?page=1&update=1401216280");
-                        string[] elems = line.Split('\n');
-                        string mess = "";
-                        for (int i = 1; i < 6; i++)
-                        {
-                            string[] first = elems[i].Split('\t');
-                            string info = await client.GetStringAsync("http://mediametrics.ru/rating/index.tsv?titles=" + first[5]);
-                            string[] elems1 = info.Split('\t');
-                            mess += elems1[1] + '\n';
-                        }
-                        mes.Add(mess + '\b');
-                        mes.Add("BСервер принял новое сообщение");
-                        App.Current.Properties["messages"] = Formats.FromListIntoString(mes);
-                        m.MakeFrame();
-                        m.MakeContent();
-                        news = new_news;
-                    }
-                    break;
-                }
-            }
-            catch
-            {
-                List<string> mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-                mes = Formats.FromStringIntoList((string)App.Current.Properties["messages"]);
-                mes.Add("BБот перегружен, подождите, пока он не восстановится");
-                App.Current.Properties["messages"] = Formats.FromListIntoString(mes);
-                m.MakeFrame();
-                m.MakeContent();
-            }
-        }
-    */
+        */
     }
 }
